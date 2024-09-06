ce645c3d86c5efff4a746e27caab047865c348b4
Nori <jiwonchichi1@gmail.com>
2024-09-06 10:02:21 +0900

    240906 10:02 파일 생성 및 정리

--- MetaversePlayground/240906Main.cpp
+++ MetaversePlayground/240906Main.cpp
@@ -18,18 +18,45 @@ int IntReturn() {
 	std::cout << "return 후" << std::endl;
 }
 
-void Func4_2() {
-	std::cout << "끝" << std::endl;
+void Func4_2() {						// 1
+	std::cout << "끝" << std::endl;		// 2
+}										// 3
+										
+void Func4_1() {						// 4
+	Func4_2();							// 5
+}										// 6
+										
+void Main() {							// 7
+	Func4_1();							// 8
+}										// 9
+// 함수 호출 순서 : 7 8 4 5 1 2 3 6 9
+
+// <함수 전방 선언>
+// 함수를 구현하기 전에 함수의 선언만 우선 진행
+// 컴파일 시 함수의 호출은 함수의 구현을 필요로 하며 구현이 없는 경우 사용 불가
+// 전방 선언을 진행하는 경우 함수의 구현을 호출 이후에 진행하도록 구현 가능
+
+void Func6_1(); // 선언은 있지만 구현이 없기에 오류 발생
+void Func6_3();
+
+// <기본 매개변수>
+// 매개변수에 기본값을 지정하여 함수 호출 시 매개변수 전달하지 '않으면' 기본값으로 처리
+// 기본 매개변수는 뒤에서부터 지정을 해야함
+void Func7(int param, int param1=2/*기본 매개변수*/, int param2 = 3) {
+
 }
 
-void Func4_1() {
-	Func4_2();
+void Main6() {
+	Func6_1();
+	Func6_3();
+	// Func6_2(); // 함수의 호출이 함수의 구현보다 먼저 진행되었기에 오류 발생
 }
 
-void Main() {
-	Func4_1();
+void Func6_2() {
+
 }
 
 int main() {
-	Main();
+	// Main();
+	Main6();
 }
