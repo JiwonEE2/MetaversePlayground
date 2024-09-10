8be0002054eaae2768ef70f1ba717e58677b0979
Nori <jiwonchichi1@gmail.com>
2024-09-10 14:55:09 +0900

    240910 14:55 빙고 테이블 셔플 함수 완성 및 확인

--- MetaversePlayground/240910Homework3.cpp
+++ MetaversePlayground/240910Homework3.cpp
@@ -4,3 +4,31 @@
 ex) 10 입력 시 10이 들어간 부분 막힘 표시
 - 종료 조건 : 빙고3
 */
+#include<iostream>
+using namespace std;
+
+void TableShuffle(int table[]) {
+	int dest, temp;
+	for (int i = 0; i < 25; i++) {
+		dest = rand() % 25;
+		temp = table[dest];
+		table[dest] = table[i];
+		table[i] = temp;
+	}
+}
+
+int main() {
+	int table[25] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24 };
+	
+	// 셔플
+	srand(time(0));
+	TableShuffle(table);
+	/* 
+	// 셔플 확인용!
+	for (int i = 0; i < 25; i++) {
+		cout << table[i] << endl;
+	}
+	*/
+
+
+}
