e5c8f90d19980c44ff8336ed9402549cdf88c83f
Nori <jiwonchichi1@gmail.com>
2024-09-11 10:27:41 +0900

    240911 10:27 파일 생성

--- MetaversePlayground/240911String.cpp
+++ MetaversePlayground/240911String.cpp
@@ -12,5 +12,44 @@ string
 using namespace std;
 
 int main() {
+	string str = "Hello";
+	string name = "kyungil";
 
+	string result;
+	cout << "스트링 클래스를 활용한 출력" << str << endl;
+	
+	// 문자열의 길이 계산(길이 반환)
+	cout << "str의 길이 : " << str.length() << endl;
+	cout << "str의 길이 : " << str.size() << endl;		// string.length() == string.size()
+
+	// 문자열 연결
+	result = str + ", " + name + "!!";
+	cout << "연결결과 : " << result << endl;
+	
+	// substr : 부분 문자열 추출 : 특정 위치에서 지정된 길이만큼 부분 문자열을 추출
+	string sub = result.substr(0, 5);
+	cout << sub << endl;
+
+	// 문자열 교체(문자열의 특정 부분을 다른 것으로 교체)
+	// 0번째 위치에서 3개의 문자를 Hi로 변경
+	result.replace(0, 3, "Hi");
+	cout << result << endl;
+
+	// string.empty()의 자료형은 bool
+	if (str.empty()) {
+		cout << "비어 있음";
+	}
+	else {
+		cout << "비어 있지 않다";
+	}
+
+	// 문자열 끝에 다른 문자열 추가
+	result.append("game");
+	cout << result << endl;
+
+	string test = "Hello World";
+	test.insert(5, "-");
+	cout << "insert : " << test << endl;
+	test.erase(5, 2);
+	cout << "erase : " << test << endl;
 }
