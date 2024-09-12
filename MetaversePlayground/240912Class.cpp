95d1169445270923ad66a7a43fd18b9787d506b6
Nori <jiwonchichi1@gmail.com>
2024-09-12 11:52:42 +0900

    240912 11:52 실습파일 생성 및 완성

--- MetaversePlayground/240912Class.cpp
+++ MetaversePlayground/240912Class.cpp
@@ -8,14 +8,30 @@ class
 ㄴ 틀을 만들었다면 써먹어야 
 ㄴ 객체는 설계도를 기반으로 실체화 시키는 것
 ㄴ 인스턴스화 : 클래스를 사용하여 객체를 생성하는 과정을 의미
+
 ==================================================
 class 자동차{
-// 멤버변수(속성->부품)
-// 멤버함수(기능->브레이크,방향지시등...)
+private:
+// 멤버변수(속성->부품->숨겨야함)
 
+public:
+// 멤버함수(기능->브레이크,방향지시등...->우리가 이용해야 함->노출)
+}
 
+==================================================
+- 접근제어지시자
+- private : 숨겨야 할 멤버변수 등을 이곳에 사용
+ㄴ 클래스의 디폴트
+- public : 외부에서 자유롭게 참조할 수 있다
+ㄴ 외부에서 직접적으로 멤버들을 조작할 수 있기 때문에 공개할 기능들은 이곳에 사용
+- protected : 상속관계...
 
+접근제어		클래스내부	클래스외부	상속클래스
+private			O			X			X
+public			O			O			O
+protected		O			X			O
 
+==================================================
 
 */
 #include<iostream>
@@ -41,6 +57,31 @@ class CCircle {
 	}
 };
 
+class Student
+{
+public:			// 접근제어
+	Student();	// 생성자
+	~Student();	// 소멸자
+
+public:
+	// 멤버함수
+	void Print() {
+		cout << "스튜던트 클래스의 프린트 함수";
+	}
+	void Print(int a, int b) {
+		cout << a << ", " << b << endl;
+	}
+	void Print(string str = "디폴트 매개변수") {
+		cout << str << endl;
+	}
+
+	// 클래스 내부에 원형만 선언
+	int Sum(int a, int b);
+
+private:		// 접근제어
+	// 멤버변수
+};
+
 int main() {
 	Circle c;
 	c.radius = 5;
@@ -48,4 +89,9 @@ int main() {
 
 	CCircle cc;
 	// cc. -> 접근 불가
+}
+
+int Student::Sum(int a, int b)
+{
+	return a + b;
 }
---
+++ MetaversePlayground/240912Exercise1.cpp
@@ -0,0 +1,22 @@
+/*	240912 실습1
+1. 사각형 클래스 만들고
+2. 가로, 세로
+3. 면적을 출력하는 함수를 만들어라
+*/
+
+#include<iostream>
+using namespace std;
+class Rectangle {
+	int height = 10;
+	int width = 20;
+public:
+	void CalculateArea() {
+		int Area = height * width;
+		cout << Area;
+	}
+};
+
+int main() {
+	Rectangle rec1;
+	rec1.CalculateArea();
+}
