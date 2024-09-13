7e754469bb22e234dc1b023c926feb98430dc42a
Nori <jiwonchichi1@gmail.com>
2024-09-13 12:10:22 +0900

    240913 12:10 과제1 시작

--- MetaversePlayground/240913Homework1.cpp
+++ MetaversePlayground/240913Homework1.cpp
@@ -1,5 +1,36 @@
 /*	240913 과제1
-1. 슬라이드 퍼즐 만들기
+[ 슬라이드 퍼즐 만들기 ]
+0. welcome ^^
+1. 상태 출력
+2. 입력 받기
+3. 움직이기
+4. puzzle class 에서 위 3가지 함수를 만들어 main의 while문을 사용하여 반복
 - 4x4 크기
 - wasd 사용
 */
+#include<iostream>
+using namespace std;
+
+class Puzzle {
+	int number[15]=
+public:
+	Puzzle() {
+		
+	}
+	void DisplayPuzzle() {
+
+	}
+	void InputKey() {
+
+	}
+	void MoveNumber() {
+
+	}
+};
+
+int main() {
+	while (true)
+	{
+
+	}
+}
