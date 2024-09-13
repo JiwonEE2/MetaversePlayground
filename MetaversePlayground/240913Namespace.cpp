92f06ddc5fb0d994ac65bf820f13fcf7b3873b37
Nori <jiwonchichi1@gmail.com>
2024-09-13 09:52:38 +0900

    240913 9:52 정리 및 파일 생성

--- MetaversePlayground/240913Namespace.cpp
+++ MetaversePlayground/240913Namespace.cpp
@@ -9,3 +9,35 @@ using namespace std;
 
 =============================================================================
 */
+namespace A {
+	void Print() {
+		cout << "나는 A공간에 살고있는 김철수";
+	}
+}
+
+namespace B {
+	void Print() {
+		cout << "나는 B공간에 살고있는 김철수";
+	}
+}
+
+namespace out {
+	namespace in {
+		void Show() {
+			cout << "dd" << endl;
+		}
+	}
+}
+
+namespace math {
+	int sum(int a, int b) {
+		return a + b;
+	}
+}
+
+int main() {
+	A::Print();
+	B::Print();
+	out::in::Show();
+	//math::sum
+}
