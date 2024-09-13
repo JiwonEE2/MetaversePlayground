ff07069a851a523b791fbcda2eb59f5f5bc7f571
Nori <jiwonchichi1@gmail.com>
2024-09-13 13:41:04 +0900

    240913 13:41 과제1 셔플, 출력, 입력까지 구현. 0의 위치 반환하여 움직이기 해야함

--- MetaversePlayground/240913Homework1.cpp
+++ MetaversePlayground/240913Homework1.cpp
@@ -1,10 +1,12 @@
 /*	240913 과제1
 [ 슬라이드 퍼즐 만들기 ]
 0. welcome ^^
-1. 상태 출력
-2. 입력 받기
-3. 움직이기
-4. puzzle class 에서 위 3가지 함수를 만들어 main의 while문을 사용하여 반복
+1. 셔플
+2. 상태 출력
+3. 0의 위치 반환하기
+4. 입력 받기
+5. 움직이기
+6. puzzle class 에서 위 4가지 함수를 만들어 main의 while문을 사용하여 반복
 - 4x4 크기
 - wasd 사용
 */
@@ -12,25 +14,84 @@
 using namespace std;
 
 class Puzzle {
-	int number[15]=
+	int number[16];
+	char key;
+
 public:
 	Puzzle() {
-		
+		for (int i = 0; i < 16; i++) {
+			number[i] = i;
+		}
 	}
+	// 1. 셔플
+	void ShufflePuzzle() {
+		int temp, ran;
+		srand(time(0));
+		for (int i = 0; i < 16; i++) {
+			ran = rand() % 16;
+			temp = number[i];
+			number[i] = number[ran];
+			number[ran] = temp;
+		}
+	}
+	// 2. 상태 출력
 	void DisplayPuzzle() {
+		for (int i = 0; i < 16; i++) {
+			cout << number[i] << "\t";
+			if (i % 4 == 3) {
+				cout << "\n";
+			}
 
+		}
 	}
+	// 3. 0의 위치 반환하기
+	int ZeroWhere() {
+		for (int i = 0; i < 16; i++) {
+			if (number[i] == 0) {
+				return i;
+			}
+		}
+	}
+	// 4. 입력 받기
 	void InputKey() {
-
+		cout << "wasd 키로 움직이세요\n";
+		cin >> key;
 	}
+	// 5. 움직이기
 	void MoveNumber() {
-
+		switch (key)
+		{
+		case 'w':			
+			break;
+		case 'a':
+			break;
+		case 's':
+			break;
+		case 'd':
+			break;
+		default:
+			break;
+		}
 	}
 };
 
 int main() {
+	// 0. 생성
+	Puzzle p;
+	// 1. 셔플
+	p.ShufflePuzzle();
+
+	// 6. 반복
 	while (true)
 	{
+		// 2. 상태 출력
+		p.DisplayPuzzle();
+		// 3. 0의 위치 받기
+
+		// 4. 입력받기
+		p.InputKey();
+
+		// 5. 움직이기
 
 	}
 }
